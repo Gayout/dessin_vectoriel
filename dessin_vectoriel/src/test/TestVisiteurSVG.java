420fb5c8bedc66b9dce510b840abc944010e1c2c
Gayout <gayoutquentin@gmail.com>
2015-11-18 11:46:55 +0100

    visit Ellipse

--- dessin_vectoriel/src/test/TestVisiteurSVG.java
+++ dessin_vectoriel/src/test/TestVisiteurSVG.java
@@ -1,19 +1,74 @@
 package test;
 
+import java.awt.Color;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import abstraction.Dessin;
+import abstraction.DessinComposite;
 import implementation.Cercle;
+import implementation.CourbeBezier;
+import implementation.Crayon;
 import implementation.Position;
+import interpreteur.svg.CourbeBezierSVG;
+import interpreteur.svg.EllipseSVG;
 import visitor.VisiteurSVGDessiner;
 
 public class TestVisiteurSVG {
-	
+
 	public static void main(String[] args) {
 		VisiteurSVGDessiner visiteur = new VisiteurSVGDessiner();
+
+		DessinComposite dessin = new DessinComposite();
+
+		Cercle cercle = new Cercle(new Position(150,150), 100, Dessin.VIDE);
+		cercle.setCrayon(new Crayon(2, Color.cyan));
 		
-		Cercle cercle = new Cercle(new Position(150,150), 100, true);
-		
-		cercle.accept(visiteur);
-		
-		System.out.println(visiteur.getCode());
+		List<Position> sommets = new ArrayList<Position>();
+
+		sommets.add(new Position(100,100));
+		sommets.add(new Position(200,200));
+		sommets.add(new Position(300,100));
+		sommets.add(new Position(300,200));
+
+		CourbeBezier courbeBezier = new CourbeBezier(sommets, Dessin.FERME, Dessin.REMPLI);
+		courbeBezier.setCrayon(new Crayon(2,Color.yellow));
+
+		EllipseSVG ellipse = new EllipseSVG(new Position(250,250),50,30,30,Dessin.VIDE);
+		ellipse.setCrayon(new Crayon(10, Color.red));
+
+		dessin.addDessin(cercle);
+		dessin.addDessin(courbeBezier);
+		dessin.addDessin(ellipse);
+
+		dessin.accept(visiteur);
+
+		try {
+
+			File file = new File("visiteur.svg");
+
+			if (!file.createNewFile()){
+				System.out.println("File already exist");
+				file.delete();
+				file = new File("visiteur.svg");
+				System.out.println("File is overwrited!");
+			}
+			else
+				System.out.println("File is created!");
+
+			FileWriter fw = new FileWriter(file.getAbsoluteFile(), true);
+			BufferedWriter bw = new BufferedWriter(fw);
+			bw.write(visiteur.getCode());
+			bw.close();
+
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
--- dessin_vectoriel/src/visitor/VisiteurSVGDessiner.java
+++ dessin_vectoriel/src/visitor/VisiteurSVGDessiner.java
@@ -14,7 +14,7 @@ public class VisiteurSVGDessiner implements Visiteur{
 		super();
 		code = "<?xml version=\"1.0\" standalone=\"no\"?>\n"
 				+ "<!DOCTYPE svg PUBLIC \"-//W3C//DTD SVG 1.1//EN\" \"http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd\">\n"
-				+ "<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"500\" height=\"500\" >\n\t</svg>";
+				+ "<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"500\" height=\"500\" >\n</svg>";
 	}
 
 	public String getCode() {
@@ -34,20 +34,24 @@ public class VisiteurSVGDessiner implements Visiteur{
 	public void visit(Cercle cercle) {
 		code = code.substring(0, code.length()-6);
 		CercleSVG cercleSVG= cercle.creerCercleSVG();
-		code += cercleSVG.generateCode();
-		code += "</svg>";
+		code += "\t"+cercleSVG.generateCode();
+		code += "\n</svg>";
 	}
 
 	@Override
 	public void visit(CourbeBezier courbeBezier) {
-		// TODO Auto-generated method stub
-		
+		code = code.substring(0, code.length()-6);
+		CourbeBezierSVG courbeBezierSVG= courbeBezier.creerCourbeBezierSVG();
+		code += "\t"+courbeBezierSVG.generateCode();
+		code += "\n</svg>";		
 	}
 
 	@Override
 	public void visit(Ellipse ellipse) {
-		// TODO Auto-generated method stub
-		
+		code = code.substring(0, code.length()-6);
+		EllipseSVG ellipseSVG= ellipse.creerEllipseSVG();
+		code += "\t"+ellipseSVG.generateCode();
+		code += "\n</svg>";		
 	}
 
 	@Override
