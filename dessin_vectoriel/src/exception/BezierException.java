a0c0c2b6b21203abd45b9a17ed1a2d3d6c123593
Gayout <gayoutquentin@gmail.com>
2015-11-23 09:47:32 +0100

    JavaDoc exception package

--- dessin_vectoriel/src/exception/BezierException.java
+++ dessin_vectoriel/src/exception/BezierException.java
@@ -1,5 +1,18 @@
 package exception;
 
+import implementation.CourbeBezier;
+import interpreteur.awt.CourbeBezierAWT;
+
+/**
+ * <b>BezierException indique à une CourbeBezierAWT qu'elle possède un nombre irregulier de points de contrôle.</b>
+ * <p>
+ * Ce nombre étant minoré par 5, toutes les Courbes de Bézier possédant plus de 4 points de contrôle ne peuvent être dessinées en AWT
+ * </p>
+ * @see CourbeBezier
+ * @see CourbeBezierAWT
+ * @author Aurore Claude/Quentin Gayout
+ *
+ */
 public class BezierException extends Exception {
 
 	/**
