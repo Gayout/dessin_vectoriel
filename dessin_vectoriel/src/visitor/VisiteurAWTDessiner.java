353705ed1d2962ba39a1a77b3f9d28a1a1f6fd55
Gayout <gayoutquentin@gmail.com>
2015-11-24 23:33:51 +0100

    remove unused import

--- dessin_vectoriel/src/visitor/VisiteurAWTDessiner.java
+++ dessin_vectoriel/src/visitor/VisiteurAWTDessiner.java
@@ -6,7 +6,6 @@ import java.awt.Font;
 import java.awt.Frame;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
-import java.awt.HeadlessException;
 import java.awt.Shape;
 
 import abstraction.Dessin;
